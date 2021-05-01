229105774db7eec443640bde5f0ce770ab55d5cc
蘇琬雅 <suwanya@mangoSuMBPro.local>
2021-05-01 18:43:00 +0800

    fix: auto play video of safari in index.html、js/homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -11,12 +11,13 @@ function createObserver() {
 			
 			//prevRatio = 1，表示 video 100% inviewport or visible
 			if (prevRatio > 0.55) {
+				entry.target.currentTime = 2;
 				entry.target.play().catch(console.log);
 			}
 		});
 
 	}, options);
-	const video = document.querySelector('#make-drinks-10s-video')
+	const video = document.querySelector('#make-drinks-10s-video');
 	observer.observe(video);   
 }
 
@@ -116,5 +117,6 @@ function modalFormHiden() {
 
 function showReelVideoPlay() {
 	const video = document.querySelector("#main-banner-video");
+	video.currentTime = 0;
 	video.play();
 }
