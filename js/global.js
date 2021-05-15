104859c91c441ef1f62e18eb0b9014a3e69e22e8
mangoSu <acid7820@gmail.com>
2021-05-15 21:32:03 +0800

    feat: move loding、submmit form methods from homepage.js to global.js

---
+++ js/global.js
@@ -0,0 +1,78 @@
+/**********************************************************
+	loading
+**********************************************************/
+window.addEventListener('load', (event) => {
+	setTimeout(() => {
+		var loading = document.querySelector('.loading-container');
+		loading.classList.add('hideLoading');
+		loading.style.zIndex = -1;
+		var main = document.querySelector('main');
+		main.classList.remove('hide');
+	}, 5000);
+	// }, 100);
+});
+
+/**********************************************************
+	Form submit
+**********************************************************/
+function initReservationForm() {
+	return {
+		name: null,
+		companyName: null,
+		taxIdNumber: null,
+		phone: null,
+		email: null,
+		contents: null
+	}
+}
+function verifyForm() {
+	let postForm = initReservationForm();
+	
+	const form = document.reservation;
+	postForm = {
+		...postForm,
+		name: form.name.value,
+		companyName: form.companyName.value,
+		taxIdNumber: form.taxIdNumber.value,
+		phone: form.phone.value,
+		email: form.email.value,
+		contents: form.contents.value
+	}
+
+	const hasEmpty = Object.keys(postForm).some(key => postForm[key] == null || postForm[key] === "")
+	if( hasEmpty ) {
+		alert('必填欄位不能為空');
+		return;
+	}
+	submmitReservationForm(postForm);
+}
+function submmitReservationForm(postForm) {
+	//時間戳記
+	const today = new Date();
+	const yyyy = today.getFullYear();
+	const mm = today.getMonth()+1;
+	const dd = today.getDate();
+	const h = today.getHours();
+	const m = today.getMinutes();
+	const s = today.getSeconds();
+	const timeStamp = `${yyyy}/${add0ToUnitsDigit(mm)}/${add0ToUnitsDigit(dd)} ${add0ToUnitsDigit(h)}:${add0ToUnitsDigit(m)}:${add0ToUnitsDigit(s)}`;
+	//時間數字為個位數，前面加 0
+	function add0ToUnitsDigit(i) {
+		if (+i < 10) {
+			i = "0" + i;
+		}
+		return i;
+	}
+
+	const url = "https://script.google.com/macros/s/AKfycbzNTRqUFN00-Y_zzX1bDaBs6_GDjPj6T80GFVKwvjn6-BKMXmPg1yshoihAfw0ucnMW/exec"
+	$.get(url, {
+		...postForm,
+		'timeStamp': timeStamp
+	});
+	alert('送出成功');
+	modalFormHiden();
+}
+var myModal = new bootstrap.Modal(document.getElementById('modalForm'))
+function modalFormHiden() {
+	myModal.hide();
+}
